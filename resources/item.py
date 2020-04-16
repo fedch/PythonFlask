f752e7e65d2ab7d93016c0951da539e26b3ebf5c
Elena Fedchenko <fedchenko.yelena@gmail.com>
2020-04-16 17:11:53 +1200

    Made nicer looking cooments.

--- resources/item.py
+++ resources/item.py
@@ -3,12 +3,14 @@ from flask_jwt import jwt_required
 from models.item import ItemModel
 
 class Item(Resource):
-    parser = reqparse.RequestParser() # only pass certain fields through JSON to our endpoints
+    # Only pass certain fields through JSON to our endpoints
+    parser = reqparse.RequestParser()
     parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
     parser.add_argument('store_id', type=int, required=True, help="Every item needs a store id.")
 
     @jwt_required()
-    def get(self, name): # accessed with GET method
+    # Accessed with GET method:
+    def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
             return item.json()
@@ -37,7 +39,8 @@ class Item(Resource):
 
         return {'message': 'Item deleted.'}
 
-    def put(self, name): # can creare new items like POST, but can ALSO modify existing items (e.g. change price in the Body in postman)
+    # Can creare new items like POST, but can ALSO modify existing items (e.g. change price in the Body in postman):
+    def put(self, name):
         data = Item.parser.parse_args()
 
         item = ItemModel.find_by_name(name)
